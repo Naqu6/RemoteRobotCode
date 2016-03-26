487dc248b9bc9e9092dbab2bd215d93ed1f5941a
Ryan Ehrlich <ryansehrlich@gmail.com>
2016-03-26 16:51:40 -0700

    Working

--- RemoteRobotCode/src/org/usfirst/frc/team8/robot/Robot.java
+++ RemoteRobotCode/src/org/usfirst/frc/team8/robot/Robot.java
@@ -1,7 +1,9 @@
 
 package org.usfirst.frc.team8.robot;
 
+import edu.wpi.first.wpilibj.networktables.*;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.TalonSRX;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -9,9 +11,16 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends IterativeRobot {
 	
+	private TalonSRX leftDrive1 = new TalonSRX(0);
+	private TalonSRX leftDrive2 = new TalonSRX(0);
+	private TalonSRX rightDrive1 = new TalonSRX(0);
+	private TalonSRX rightDrive2 = new TalonSRX(0);
 
+	private NetworkTable hardware = NetworkTable.getTable("Hardware");
+
+	
     public void robotInit() {
-
+    	
     }
   
     public void autonomousInit() {
@@ -21,10 +30,18 @@ public class Robot extends IterativeRobot {
   
     public void autonomousPeriodic() {
     }
+    
+    public void teleopInit() {
+    	
+    }
 
 
     public void teleopPeriodic() {
+        leftDrive1.set(hardware.getNumber("left1", 0));
+        leftDrive2.set(hardware.getNumber("left2", 0));
         
+        rightDrive1.set(hardware.getNumber("right1", 0));
+        rightDrive2.set(hardware.getNumber("right2", 0));
     }
     
 
